78781899f7ac607e204963bdd9be4df1f061091f
Vineet Singh <127892265+Vineet-Singh0@users.noreply.github.com>
2024-08-18 22:08:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 let currentPage = 1;
 
-function toggleClass(e, toggleClassName) {
-  if (e.className.includes(toggleClassName)) {
-    e.className = e.className.replace(" " + toggleClassName, "");
+function toggleClass(element, toggleClassName) {
+  if (element.classList.contains(toggleClassName)) {
+    element.classList.remove(toggleClassName);
   } else {
-    e.className += " " + toggleClassName;
+    element.classList.add(toggleClassName);
   }
 }
 
-function movePage(e, page) {
-  if (page == currentPage) {
+function movePage(element, page) {
+  if (page === currentPage) {
     currentPage += 2;
-    toggleClass(e, "left-side");
-    toggleClass(e.nextElementSibling, "left-side");
-  } else if (page == currentPage - 1) {
+    toggleClass(element, "left-side");
+    toggleClass(element.nextElementSibling, "left-side");
+  } else if (page === currentPage - 1) {
     currentPage -= 2;
-    toggleClass(e, "left-side");
-    toggleClass(e.previousElementSibling, "left-side");
+    toggleClass(element, "left-side");
+    toggleClass(element.previousElementSibling, "left-side");
   }
 }
 
-function handleTouch(e) {
-  e.preventDefault();
+function handleTouchStart(e) {
   const touch = e.touches[0];
   const pageElement = document.elementFromPoint(touch.clientX, touch.clientY);
 
-  if (pageElement) {
-    const page = Array.from(pageElement.parentNode.children).indexOf(pageElement) + 1;
-    movePage(pageElement, page);
+  if (pageElement && pageElement.classList.contains('page')) {
+    const pageIndex = Array.from(pageElement.parentNode.children).indexOf(pageElement) + 1;
+    movePage(pageElement, pageIndex);
   }
 }
 
 document.querySelectorAll('.page').forEach(pageElement => {
-  pageElement.addEventListener('click', (e) => movePage(e.currentTarget, Array.from(e.currentTarget.parentNode.children).indexOf(e.currentTarget) + 1));
-  pageElement.addEventListener('touchstart', handleTouch, { passive: false });
+  pageElement.addEventListener('click', (e) => {
+    const pageIndex = Array.from(e.currentTarget.parentNode.children).indexOf(e.currentTarget) + 1;
+    movePage(e.currentTarget, pageIndex);
+  });
+  
+  pageElement.addEventListener('touchstart', handleTouchStart, { passive: true });
 });
