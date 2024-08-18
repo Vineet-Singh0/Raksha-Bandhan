1bfe7d55020a04be98cfc3e42e1184037a964a43
Vineet Singh <127892265+Vineet-Singh0@users.noreply.github.com>
2024-08-18 21:58:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,9 +13,25 @@ function movePage(e, page) {
     currentPage += 2;
     toggleClass(e, "left-side");
     toggleClass(e.nextElementSibling, "left-side");
-  } else if ((page = currentPage - 1)) {
+  } else if (page == currentPage - 1) {
     currentPage -= 2;
     toggleClass(e, "left-side");
     toggleClass(e.previousElementSibling, "left-side");
   }
 }
+
+function handleTouch(e) {
+  e.preventDefault();
+  const touch = e.touches[0];
+  const pageElement = document.elementFromPoint(touch.clientX, touch.clientY);
+
+  if (pageElement) {
+    const page = Array.from(pageElement.parentNode.children).indexOf(pageElement) + 1;
+    movePage(pageElement, page);
+  }
+}
+
+document.querySelectorAll('.page').forEach(pageElement => {
+  pageElement.addEventListener('click', (e) => movePage(e.currentTarget, Array.from(e.currentTarget.parentNode.children).indexOf(e.currentTarget) + 1));
+  pageElement.addEventListener('touchstart', handleTouch, { passive: false });
+});
